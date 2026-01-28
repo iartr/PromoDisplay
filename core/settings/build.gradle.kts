448edce1fce287b279eefac4acc48670c3c29160
Eugene <filipok467@mail.ru>
2026-01-28 15:12:34 +0500

    fix naming

--- core/settings/build.gradle.kts
+++ core/settings/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.settings"
+    namespace = "ru.offerfactory.promodisplay.settings"
     compileSdk = 36
 
     defaultConfig {
@@ -24,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
