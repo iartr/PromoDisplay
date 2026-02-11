a49c4ba7e3423f6c317f440844a4b339d3b0479d
Oriro_1 Eugene <filipok467@mail.ru>
2026-02-11 21:25:45 +0500

    feat(settings): add di

---
+++ core/settings/src/main/java/ru/offerfactory/promodisplay/settings/di/SettingsModule.kt
@@ -0,0 +1,14 @@
+package ru.offerfactory.promodisplay.settings.di
+
+import dagger.Binds
+import dagger.Module
+import ru.offerfactory.promodisplay.settings.ConfigManager
+import ru.offerfactory.promodisplay.settings.ConfigManagerImpl
+import javax.inject.Singleton
+
+@Module
+abstract class SettingsModule {
+    @Binds
+    @Singleton
+    abstract fun bindConfigManager(configManagerImpl: ConfigManagerImpl): ConfigManager
+}
