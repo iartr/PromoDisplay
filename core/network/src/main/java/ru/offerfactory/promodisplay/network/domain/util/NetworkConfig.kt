71427662ced352d2ae88021330f14d9e53f659ec
Serokris <xeller2021@mail.ru>
2026-06-08 02:30:05 +0300

    Переход на новый base url

--- core/network/src/main/java/ru/offerfactory/promodisplay/network/domain/util/NetworkConfig.kt
+++ core/network/src/main/java/ru/offerfactory/promodisplay/network/domain/util/NetworkConfig.kt
@@ -2,5 +2,5 @@ package ru.offerfactory.promodisplay.network.domain.util
 
 object NetworkConfig {
     const val BASE_URL =
-        "https://wlzywiojhnktpabjyvii.supabase.co/functions/v1/"
+        "https://pyacademy.tech/functions/v1/"
 }
